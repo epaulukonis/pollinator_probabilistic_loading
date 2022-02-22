96c58bb8aa5eab487eab32eb42e3805383446299
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2022-02-22 18:43:03 -0500

    Update 03_vectorization_fields.R

--- src/AdjustedAnalysis/03_vectorization_fields.R
+++ src/AdjustedAnalysis/03_vectorization_fields.R
@@ -8,23 +8,24 @@ print("stepping into 03_vectorization_fields.R")
 
 
 
-
-#Break list into 1st 11 years and later 11 years
-set1<-cdl_data_ill_rec[[c(1:11)]]
-
-#gotta partition
+set1<-stack(cdl_data_ill_rec[c(1:2)])
+set2<-stack(cdl_data_ill_rec[c(12:22)])
 
 
 #Stack the rasters and convert to data frame
-rast_df<- as.data.frame(stack(rast1, rast2))
+rast_df<- as.data.frame(set1)
+colnames(rast_df)<-c("y1","y2")
 
 #Concatenate using the layer stack (layer.1 for rast1, layer.2 for rast2)
-rast_con<- transform(rast_df, layer.3=paste(layer.1, layer.2, sep = ""))
+rast_con<- transform(rast_df, s1=paste(y1, y2, sep = ""))
 
 #Inspect layer.3 values
 head(rast_con)
 
 #convert to matrix
-rast_mat<- as.matrix(as.numeric(as.character(rast_con$layer.3)))
+rast_mat<- as.matrix(as.numeric(as.character(rast_con$y1)))
 #convert back to raster
-rast_final<- raster(rast_elf, xmx=xmx, xmn=xmn, ymx=ymx, ymn=ymn)
+rast_final<- raster(rast_mat, xmn=406065, xmx=727725, ymn=1688025, ymx=2194845)
+
+plot(rast_final)
+head(rast_mat)
