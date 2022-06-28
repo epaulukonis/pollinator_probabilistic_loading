bba6fcaf48954589c9f8b316b76613a158839cfd
Annie P <elizabethpaulukonis@gmail.com>
2022-06-28 13:44:10 -0400

    Update 03_CPAA_threshold_c.R

--- src/ComparisonAnalysis/03_CPAA_threshold_c.R
+++ src/ComparisonAnalysis/03_CPAA_threshold_c.R
@@ -8,11 +8,10 @@ print("stepping into 03_CPAA_threshold_c.R")
 
 ##### COUNTY LAYER PREP ####
 options(scipen = 999) #remove exponent options, throws R off
-memory.limit()
-memory.limit(size=112000)
 
 
-#### Michigan
+
+#### Michigan ----
 #function to mask and crop CDL to each county
 study<-mi
 
@@ -29,8 +28,6 @@ for (co in 1:length(study)){
 
 
 #county_set_list contains the clipped sets of CDLs for each set of years
-
-
 #### CROP DIVERSITY ####
 
 #I chose these based on visual inspection; they have crop diversity > 1, and tree crops
@@ -121,10 +118,9 @@ for(layer in 1:length(county_list)){
 
 
 ##### CALCULATE THRESHOLD ####
-thresh_list<-list()
-thresh_list_raw<-list() #documents the exact year of the cutoff
+thresh_list_mi<-list()
+thresh_list_raw_mi<-list() #documents the exact year of the cutoff
 for(item in 1:length(county_list)){
-  item<-1
 county<-county_list[[item]]
 y<-county
 s0 = brick(y)
@@ -157,31 +153,25 @@ n_pixels$sample_p<-n_pixels$Freq/total_n
 
 average_1 = average_list_focus[[item]]
 thresh<-as.data.frame(n_pixels[which.min(abs(average_1-n_pixels$total)),]) #which threshold is closest to the average pesticide area?
-thresh_list_raw[[item]]<-thresh
+thresh_list_raw_mi[[item]]<-thresh
 
 #get the bin numbers that coincide with the threshold or greater
 thresh_layers<-county_binned[county_binned$bin >= (as.numeric(thresh$Var1)-1),] 
 unique(thresh_layers$bin) #double check that it looks good
 thresh_layers$bin_f<-1  #if you want binary layer
 
-thresh_list[[item]]<-thresh_layers
+thresh_list_mi[[item]]<-thresh_layers
 
 }
-
-
-
-##for low: min bin is 1
-##for med: min bin is 6
-##for high: min bin is 11
 
 
 #####Get NLCD mask for non-crop areas (roadS)----
 nlcd<-raster(paste0(nlcd_dir,"/NLCD_2019_Land_Cover_L48_20210604_8Jzq7uEvh4Wq2TtvZWFJ.tiff"))
 #nlcd<-raster(paste0(nlcd_dir,"/NLCD_2008_Land_Cover_L48_20210604_8Jzq7uEvh4Wq2TtvZWFJ.tiff"))
 
-list_of_nlcd_masks<-list()
+list_of_nlcd_masks_mi<-list()
 for (county in 1:3){
-cpaa<-three_county_list[[county]][[1]] ##get associated counties
+cpaa<-county_list[[county]][[1]] ##get associated counties
 ext<-extent(cpaa)
 #plot(cpaa)
 
@@ -199,8 +189,198 @@ nlcdc[nlcdc==24]<-NA
 nlcdc[nlcdc==31]<-NA
 
 
-list_of_nlcd_masks[[county]]<-nlcdc
+list_of_nlcd_masks_mi[[county]]<-nlcdc
 
+}
+
+#writeRaster(nlcd, file.path(cdl_dir, "/nlcd_mask.tif"), format="GTiff", overwrite = TRUE)
+
+
+
+
+
+
+
+#### Wisconsin ----
+#function to mask and crop CDL to each county
+study<-wi
+
+county_set_list<-list()
+for (co in 1:length(study)){
+  co_r<-study[study$NAME == study$NAME[co],]
+  mask_crop<-function(x){
+    r_list<-crop(x, co_r)
+    mask(r_list, co_r)
+  }
+  county_set<-lapply(cdl_data_mi_rec, mask_crop) #crop and mask the fixed CDL to the counties, put in list
+  county_set_list[[co]]<-stack(county_set) #represents single county stack
+}
+
+
+#county_set_list contains the clipped sets of CDLs for each set of years
+#### CROP DIVERSITY ####
+
+#I chose these based on visual inspection; they have crop diversity > 1, and tree crops
+names(county_set_list)<-wi$NAME
+rock<-county_set_list$Rock#65% crop coverage
+wau<-county_set_list$Waushara #35%
+lang<-county_set_list$Langdale #10-15%
+
+
+#### you can use this section to look at which crops specifically have higher than 3% acreage in the states of interest
+out_rock<-freq(rock)
+out_wau<-freq(wau)
+out_lang<-freq(lang)
+
+out<-out_rock
+prop_list<-list()
+for(i in 1:length(out)){
+  x<-out[[i]]
+  x<-na.omit(x)
+  prop<-as.data.frame(x[,2]/sum(x[,2]))
+  names(prop)<-'proportion'
+  prop$value<-x[,1]
+  prop_list[[i]]<-prop
+}
+
+
+diversity_function<-function(x){
+  out<-which(x[,1] >= 0.03)
+  x$row<-as.numeric(row.names(x))
+  crops<-x[x$row %in% out,]
+}
+
+crop_list_rock<-lapply(prop_list,diversity_function)
+print(crop_list_rock) #what's the general trend in crops? 
+names(crop_list_rock)<-2008:2021
+
+
+chosen_count<-list(hurock, wau, lang)
+
+
+
+##### YEARLY AVERAGE LAYERS#### 
+##get average area for threshold here
+#this reclassifys all of the non-major crops
+is_n <- c(0,1:256)
+becomes_n <- c(NA,rep.int(1,256))
+n<-cbind(is_n,becomes_n)
+
+
+#this reclassifys the layers from county_set_list to be binary (non-diverse crop and diverse crop)
+county_list<-list()
+layer_list<-list()
+for (county in 1:length(chosen_count)){
+  county_r<-chosen_count[[county]]
+  for(layer in 1:nlayers(county_r)){
+    layer_list[[layer]] <- reclassify(county_r[[layer]], n)
+  }
+  county_list[[county]]<-layer_list
+}
+#rm(county_r)
+
+
+
+average_list_focus<-list()
+for(layer in 1:length(county_list)){
+  county<-county_list[[layer]]
+  list_freq<-list()
+  for(f in 1:length(county)){
+    sq<-as.data.frame(freq(county[[f]]))
+    sq[2,1]<-0
+    list_freq[[f]]<-sq
+  }
+  #this should unlist the frequency set and put it in a data-frame that we can then use to evaluate 'core' crop area
+  cdl_crop_areas<-do.call(cbind, lapply(list_freq, as.data.frame))
+  value<-cdl_crop_areas$value
+  cdl_crop_areas = cdl_crop_areas[,!names(cdl_crop_areas) == 'value']
+  cdl_crop_areas$value<-value
+  cdl_crop_areas[,1:14]<-(cdl_crop_areas[,1:14]*900) * 0.000247105 #get area in acres
+  names(cdl_crop_areas)<-c(2008:2021,"crop") #make sure names align
+  extracted_field_list[[layer]]<-cdl_crop_areas
+  
+  average_1<-rowMeans(cdl_crop_areas[1,2:14]) #average of crops year by year
+  average_0<-rowMeans(cdl_crop_areas[10,2:14]) #average of 'other' year by year
+  average_list_focus[[layer]]<-average_1
+  
+}
+
+
+##### CALCULATE THRESHOLD ####
+thresh_list_wi<-list()
+thresh_list_raw_wi<-list() #documents the exact year of the cutoff
+for(item in 1:length(county_list)){
+  county<-county_list[[item]]
+  y<-county
+  s0 = brick(y)
+  coords = coordinates(s0) 
+  s1 = as.data.frame(getValues(s0))
+  layer.3 = sapply(1:nrow(s1), function(x) paste0(s1[x, ], collapse = ''))
+  layer.3 <- data.frame(coords, layer.3)
+  
+  county_binned<-layer.3
+  county_binned <- county_binned[!grepl("NANANANANANANANANANANA", county_binned$layer.3),] # remove pixels that have no crops in 11 years
+  county_binned$f<-gsub("NA", "", county_binned$layer.3) #substitute "" for NA; this will preserve order value, this bins pixels by # years cropped (no differentiation between when)
+  county_binned$field<-as.numeric(county_binned$f) #turn f into fields as numeric integer
+  county_binned$n_years<-as.numeric(gsub(0, "", county_binned$field))
+  county_binned$bin<-floor(log10(county_binned$n_years)) + 1  #create column to count the number of years
+  
+  #N pixels
+  county_binned$bin<-ifelse(county_binned$field == 0, 0, county_binned$bin)
+  n_pixels<-as.data.frame(table(county_binned$bin))
+  # p<-ggplot(n_pixels, aes(x=Var1, y=Freq)) +
+  #   geom_bar(stat="identity")
+  # p
+  n_pixels$total<-NA
+  for(i in nrow(n_pixels):2){
+    n_pixels[i,3]<-(sum(n_pixels[i:14,2])*900)*0.000247105
+    # n_pixels[i,3]<-sum(n_pixels[1:i,2])
+  }
+  
+  total_n<-sum(n_pixels$Freq)
+  n_pixels$sample_p<-n_pixels$Freq/total_n
+  
+  average_1 = average_list_focus[[item]]
+  thresh<-as.data.frame(n_pixels[which.min(abs(average_1-n_pixels$total)),]) #which threshold is closest to the average pesticide area?
+  thresh_list_raw_wi[[item]]<-thresh
+  
+  #get the bin numbers that coincide with the threshold or greater
+  thresh_layers<-county_binned[county_binned$bin >= (as.numeric(thresh$Var1)-1),] 
+  unique(thresh_layers$bin) #double check that it looks good
+  thresh_layers$bin_f<-1  #if you want binary layer
+  
+  thresh_list_wi[[item]]<-thresh_layers
+  
+}
+
+
+
+#####Get NLCD mask for non-crop areas (roadS)----
+nlcd<-raster(paste0(nlcd_dir,"/NLCD_2019_Land_Cover_L48_20210604_8Jzq7uEvh4Wq2TtvZWFJ.tiff"))
+#nlcd<-raster(paste0(nlcd_dir,"/NLCD_2008_Land_Cover_L48_20210604_8Jzq7uEvh4Wq2TtvZWFJ.tiff"))
+
+list_of_nlcd_masks_wi<-list()
+for (county in 1:3){
+  cpaa<-county_list[[county]][[1]] ##get associated counties
+  ext<-extent(cpaa)
+  #plot(cpaa)
+  
+  #use 2008 NLCD to mask out non-crop
+  
+  nlcdc<-projectRaster(nlcd, cpaa, method='ngb',crs(cpaa))
+  nlcdc<-crop(nlcdc, cpaa)
+  nlcdc<-mask(nlcdc, cpaa)
+  
+  nlcdc[nlcdc==11]<-NA
+  nlcdc[nlcdc==21]<-NA
+  nlcdc[nlcdc==22]<-NA
+  nlcdc[nlcdc==23]<-NA
+  nlcdc[nlcdc==24]<-NA
+  nlcdc[nlcdc==31]<-NA
+  
+  
+  list_of_nlcd_masks_wi[[county]]<-nlcdc
+  
 }
 
 #writeRaster(nlcd, file.path(cdl_dir, "/nlcd_mask.tif"), format="GTiff", overwrite = TRUE)
