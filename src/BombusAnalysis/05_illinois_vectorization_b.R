1a030a9a090d5db34e0a7c95b63ef3d3ea3795c6
Annie P <elizabethpaulukonis@gmail.com>
2023-03-09 13:39:19 -0500

    update

---
+++ src/BombusAnalysis/05_illinois_vectorization_b.R
@@ -0,0 +1,96 @@
+### Probabilistic Crop Loading 
+
+### 04 getting sub-delineations
+
+# Edited by E. Paulukonis March 2023
+
+#in this script, we process the final field outputs and extract the original CDL to the fields while applying a get of geometric
+#functions to match original crop patterns by year. we then output the final field delineation as a vector for each year
+
+print(list.files(path=paste0(root_data_out, "/all_tif/ILLINOIS/bombus/SUB"), pattern='.shp$', all.files=TRUE, full.names=FALSE))
+ill_field<- file.path(paste0(root_data_out, "/all_tif/ILLINOIS/bombus/SUB"), list.files(path=paste0(root_data_out, "/all_tif/ILLINOIS/bombus/SUB"), pattern='.shp$', all.files=TRUE, full.names=FALSE))
+ill_field<-setNames(lapply(ill_field, readOGR), tools::file_path_sans_ext(basename(ill_field)))
+ill_fieldf<-ill_field[(mixedsort(as.character(names(ill_field))))]
+
+#to match appropriate NLCD...
+#sets of CDL years
+#1999-2002: 2001
+#2003-2006: 2006
+#2007-2009: 2008
+#2010-2012: 2011
+#2013-2014: 2013
+#2015-2017: 2016
+#2018-2021: 2019
+
+
+temporal_level_vectors<-list()
+for(i in 1:length(field_list_ill_f)){
+  i=1
+  fields<-ill_fieldf[[temp]]
+  
+  #match cdl sets 
+    if(i ==1){cdl_data<-cdl_data_ill_rec[1:4]}
+    if(i ==2){cdl_data<-cdl_data_ill_rec[5:8]}
+    if(i ==3){cdl_data<-cdl_data_ill_rec[9:11]}
+    if(i ==4){cdl_data<-cdl_data_ill_rec[12:14]}
+    if(i ==5){cdl_data<-cdl_data_ill_rec[15:16]}
+    if(i ==6){cdl_data<-cdl_data_ill_rec[17:19]}
+    if(i ==7){cdl_data<-cdl_data_ill_rec[20:23]}
+  
+  #match names of cdl sets
+    if(i ==1){names(cdl_data)<-names(cdl_data_ill_rec[1:4])}
+    if(i ==2){names(cdl_data)<-names(cdl_data_ill_rec[5:8])}
+    if(i ==3){names(cdl_data)<-names(cdl_data_ill_rec[9:11])}
+    if(i ==4){names(cdl_data)<-names(cdl_data_ill_rec[12:14])}
+    if(i ==5){names(cdl_data)<-names(cdl_data_ill_rec[15:16])}
+    if(i ==6){names(cdl_data)<-names(cdl_data_ill_rec[17:19])}
+    if(i ==7){names(cdl_data)<-names(cdl_data_ill_rec[20:23])}
+  
+  
+field_extractions_cdl<-list()
+  for(f in 1:length(cdl_data)){
+    output<-exact_extract(cdl_data[[f]],fields,"mode")
+    output<-as.data.frame(output)
+    field_extract<-as.data.frame(terra::area(fields), na.rm=T)
+    cdl_to_fields<-as.data.frame(cbind(output, field_extract))
+    names(cdl_to_fields)<-c("Crop","Area")
+    field_extractions_cdl[[f]]<-cdl_to_fields
+  }
+
+  names(field_extractions_cdl)<-names(cdl_data)
+  temporal_level_vectors[[i]]<-field_extractions_cdl
+
+}
+
+#here, we take the extracted fields 
+
+set1<-st_as_sf(ill_fieldf[[1]])
+combo1<-cbind(set1, temporal_level_vectors[[1]][[1]])
+
+colnames(combo1)
+colnames(combo1)[3]<-"crop"
+colnames(combo1)[4]<-"area"
+combo1$id<-as.numeric(row.names(combo1))+1
+
+inst_list<-st_intersects(combo1)
+library(igraph)
+g = graph.adjlist(inst_list)
+c = components(g)
+
+test<-table(c$membership)
+test<-as.data.frame(cbind(c$membership, 1:(nrow(combo1)-1)))
+names(test)<-c("groups","id")
+combo1<-merge(combo1,test,by="id")
+
+test_combo1<-combo1 %>%
+  group_by(groups, crop) %>%
+  summarise(geometry = sf::st_union(geometry)) %>%
+  ungroup()
+
+area_thresh <- units::set_units(30, m^2) #Fill holes, just one pixel wide
+fw_fill<- fill_holes(test_combo1, threshold = area_thresh)
+
+st_write(fw_fill, root_data_out, layer="/all_tif/ILLINOIS/bombus/test_fieldoutput.shp", driver = "ESRI Shapefile")
+
+
+
