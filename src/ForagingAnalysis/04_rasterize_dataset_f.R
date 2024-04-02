e65a77681a4c8791128a896949b6612f65105adf
Paulukonis <Paulukonis.Elizabeth@epa.gov>
2024-04-02 12:12:38 -0400

    run final set of sims

--- src/ForagingAnalysis/04_rasterize_dataset_f.R
+++ src/ForagingAnalysis/04_rasterize_dataset_f.R
@@ -88,7 +88,7 @@ scenario_clip_off30<-scenario_clip_off30[,c(1:13,ncol(scenario_clip_off30),14:(n
 scenario_clip_off60<-scenario_clip_off60[,c(1:13,ncol(scenario_clip_off60),14:(ncol(scenario_clip_off60)-1))]
 scenario_clip_off90<-scenario_clip_off90[,c(1:13,ncol(scenario_clip_off90),14:(ncol(scenario_clip_off90)-1))]
 
-print(paste0("this is simulation ", sim+501))
+print(paste0("this is simulation ", sim+751))
 print(paste0("this is compound ",unique(scenario_clip_on$Compound) ))
 
 
@@ -495,8 +495,8 @@ for(sim in 1:length(final_on_field_history_list)){
 
 
       for(n in 1:length(dailymediasets_by_compound_fin)){
-        write.csv(dailymediasets_by_compound_fin[[n]], paste0(root_data_out,'/all_forage/fixed_media_tables/501-750/',names(dailymediasets_by_compound_fin[n]),sim+501, '.csv')  , row.names=F)
-        print(paste0("simulation ", names(dailymediasets_by_compound_fin[n])," ", sim+501," is done"))
+        write.csv(dailymediasets_by_compound_fin[[n]], paste0(root_data_out,'/all_forage/fixed_media_tables/751-1000/',names(dailymediasets_by_compound_fin[n]),sim+751, '.csv')  , row.names=F)
+        print(paste0("simulation ", names(dailymediasets_by_compound_fin[n])," ", sim+751," is done"))
         
       }
 
